5fe3c9033f90e3c134259927cd47a63b3bd058b2
erikaosgue <1476@holbertonschool.com>
2021-02-09 23:22:47 -0500

    Add config folder that contains the routes.rb

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+# Here all the http request arrive and get match, if so they will be sent to the controller
+# in the app/controllers folder, will find the file that matches the name of the controller
+# 
+
+Rails.application.routes.draw do
+  # get "route" to: "controller#action"
+
+  # Get /about http.1.1
+  get "about", to: "about#index"
+
+  get "sing_up", to: "registrations#new"
+  post "sing_up", to: "registrations#create"
+  
+  root to: "main#index"
+  # get "/", to: "main#index"
+end
